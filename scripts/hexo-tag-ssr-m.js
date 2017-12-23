9eb3c56c80a755e170fd965e5fe8393c8f3f776b
MasuqaT <occar@hotmail.co.jp>
2017-12-23 15:50:58 +0900

    Added `slideshare` tag & descriptions.

--- scripts/hexo-tag-ssr-m.js
+++ scripts/hexo-tag-ssr-m.js
@@ -4,11 +4,24 @@ const MACROS = {
     "\\boldsymbol": "\\mathbf{#1}" // temporary
 };
 
+/**
+ * Inline-Math tag
+ *
+ * Syntax:
+ *   {%m <math>%}
+ */
 hexo.extend.tag.register('m', function (args) {
     var math = args.join(' ');
     return katex.renderToString(math, {displayMode: false, macros: MACROS});
 });
 
+/**
+ * Display-Math tag
+ * Syntax:
+ *   {% md %}
+ *   <math>+
+ *   {% endmd %}
+ */
 hexo.extend.tag.register('md', function (args, content) {
     var math = content;
     return katex.renderToString(math, {displayMode: true, macros: MACROS});
